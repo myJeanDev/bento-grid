f871b52de82d8db15df896f78cc6701b33d93479
Willeujeans <112419867+Willeujeans@users.noreply.github.com>
2025-05-30 16:16:58 -0600

    Add flex items and flex grid

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <main className="App">
+      <div className="css-grid-8x4">
+        <div className="flex-item" style={{ gridColumn: "span 2", gridRow: "span 4" }}>a</div>
+        <div className="flex-item" style={{ gridColumn: "span 2", gridRow: "span 4" }}>b</div>
+        <div className="flex-item" style={{ gridColumn: "span 4", gridRow: "span 4" }}>c</div>
+        <div className="flex-item" style={{ gridColumn: "span 4", gridRow: "span 8" }}>d</div>
+        <div className="flex-item" style={{ gridColumn: "span 4", gridRow: "span 4" }}>e</div>
+        <div className="flex-item" style={{ gridColumn: "span 2", gridRow: "span 4" }}>a</div>
+        <div className="flex-item" style={{ gridColumn: "span 2", gridRow: "span 4" }}>a</div>
+        <div className="flex-item" style={{ gridColumn: "span 8", gridRow: "span 4" }}>a</div>
+      </div>
+    </main>
   );
 }
 
