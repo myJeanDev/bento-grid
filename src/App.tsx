055068edb9966cdc02f92554475f127aaa7e6dc0
Willeujeans <112419867+Willeujeans@users.noreply.github.com>
2025-05-30 20:13:23 -0600

    Add style to bento box grid

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import './App.css';
 function App() {
   return (
     <main className="App">
-      <div className="css-grid-8x4">
-        <div className="flex-item" style={{ gridColumn: "span 2", gridRow: "span 4" }}>a</div>
-        <div className="flex-item" style={{ gridColumn: "span 2", gridRow: "span 4" }}>b</div>
+      <div className="css-grid-8x8">
+        {/* X: gridColumn , Y: gridRow */}
+        <div className="flex-item f-row" style={{ gridColumn: "span 2", gridRow: "span 1" }}>a</div>
+        <div className="flex-item" style={{ gridColumn: "span 2", gridRow: "span 2" }}>b</div>
         <div className="flex-item" style={{ gridColumn: "span 4", gridRow: "span 4" }}>c</div>
-        <div className="flex-item" style={{ gridColumn: "span 4", gridRow: "span 8" }}>d</div>
+        <div className="flex-item" style={{ gridColumn: "span 2", gridRow: "span 1" }}>d</div>
         <div className="flex-item" style={{ gridColumn: "span 4", gridRow: "span 4" }}>e</div>
-        <div className="flex-item" style={{ gridColumn: "span 2", gridRow: "span 4" }}>a</div>
-        <div className="flex-item" style={{ gridColumn: "span 2", gridRow: "span 4" }}>a</div>
-        <div className="flex-item" style={{ gridColumn: "span 8", gridRow: "span 4" }}>a</div>
+        <div className="flex-item" style={{ gridColumn: "span 4", gridRow: "span 6" }}>f</div>
+        <div className="flex-item" style={{ gridColumn: "span 2", gridRow: "span 4" }}>g</div>
+        <div className="flex-item" style={{ gridColumn: "span 2", gridRow: "span 4" }}>h</div>
       </div>
     </main>
   );
